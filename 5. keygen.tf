6adff2fa3679a34b39b1185ef58a4984f3d6085f
Shohag Rana <shohagrana64@gmail.com>
2023-06-07 13:20:19 +1000

    Create 5. keygen.tf

---
+++ 5. keygen.tf
@@ -0,0 +1,13 @@
+#Create key-pair for logging into EC2 in us-east-1
+resource "aws_key_pair" "master-key" {
+  provider   = aws.region-master
+  key_name   = "jenkins"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+#Create key-pair for logging into EC2 in us-west-2
+resource "aws_key_pair" "worker-key" {
+  provider   = aws.region-worker
+  key_name   = "jenkins"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
